a327fccd453966d9bc9acd7599b13b7e9b571aa9
Parames8803 <parameswaran8803@gmail.com>
2025-08-15 11:10:37 +0530

    feat: Update secure store methods in supabaseClient and add expo-dev-client dependency

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,17 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
-import * as SecureStore from 'expo-secure-store';
+import { getItemAsync, setItemAsync, deleteItemAsync } from 'expo-secure-store';
 import { AppState } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
-    return SecureStore.getItemAsync(key);
+    return getItemAsync(key);
   },
   setItem: async (key: string, value: string) => {
-    await SecureStore.setItemAsync(key, value);
+    await setItemAsync(key, value);
   },
   removeItem: async (key: string) => {
-    await SecureStore.deleteItemAsync(key);
+    await deleteItemAsync(key);
   },
 };
 
